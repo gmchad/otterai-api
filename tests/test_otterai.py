8c4d509044a4f6bdc721c847adcbc4857c6f3bab
ChocoTonic <nutspizza@gmail.com>
2025-11-15 12:06:16 -0800

    Add new OtterAI tests for user ID and speech

--- tests/test_otterai.py
+++ tests/test_otterai.py
@@ -5,3 +5,19 @@ def test_otterai_instantiation():
     otter = OtterAI()
     assert otter._userid is None
     assert otter._is_userid_invalid() is True
+
+
+def test_is_userid_invalid_true():
+    otter = OtterAI()
+    assert otter._is_userid_invalid() is True
+
+
+def test_otterai_valid_userid():
+    otter = OtterAI()
+    otter._userid = "validid"
+    assert otter._is_userid_invalid() is False
+
+
+def test_stop_speech():
+    otter = OtterAI()
+    otter.stop_speech()
